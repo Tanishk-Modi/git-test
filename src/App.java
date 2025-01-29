3f9b8d7b932dae5d2d90ddd9c4912e01f187dfde
tanishkmodi <tanishkmodi6@gmail.com>
2025-01-29 16:34:26 -0500

    second commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("TEST 1");
+        System.out.println("tEST2");
+        System.out.println("test3");
     }
 }
